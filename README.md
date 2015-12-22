950b68a4e8adcff7099209ae8af1be5c8355be70
angelopublio <angelo@angelopublio.com>
2015-12-22 12:46:33 -0200

    First version :)

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
 # sharpspring-api
-Super-simple, minimum abstraction SharpSpring API v1 wrapper, in PHP.
+Super-simple SharpSpring API v1 wrapper, in PHP.
+
+Let's improve your website with Marketing Automation and Inbound Marketing.
 
 by [Angelo Públio](https://angelopublio.com)
+
+
+
+### Declare your accountID and secretKEY
+
+```
+if ( !defined('SHARPSPRING_ACCOUNTID') )
+	define('SHARPSPRING_ACCOUNTID', 'YOUR-ACCOUNT-ID');
+		
+if ( !defined('SHARPSPRING_SECRETKEY') )
+	define('SHARPSPRING_SECRETKEY', 'YOUR-SECRET-KEY');
+
+```
+
+### Include the lib
+
+Example using the folder '/lib/sharpspring-api/src/' inside the WordPress' theme folder
+
+```
+if (!class_exists('AngeloPublio\SharpSpring'))
+	require_once(get_template_directory().'/lib/sharpspring-api/src/SharpSpring.php');
+```
+
+
+### Calling the API
+
+Example calling getLeads
+
+```
+$SharpSpring = new \AngeloPublio\SharpSpring(SHARPSPRING_ACCOUNTID, SHARPSPRING_SECRETKEY);
+
+$limit = 500;                                                                         
+$offset = 0;    
+   
+$result = $SharpSpring->call('getLeads', 
+				array('where' => array('emailAddress' => 'email-to-filter@test.com'), 'limit' => $limit, 'offset' => $offset)
+			);
+print_r($result);
+
+```
+
+Example calling getFields
+
+```
+$SharpSpring = new \AngeloPublio\SharpSpring(SHARPSPRING_ACCOUNTID, SHARPSPRING_SECRETKEY);
+
+$limit = 500;                                                                         
+$offset = 0;    
+   
+$result = $SharpSpring->call('getFields', 
+				array('where' => array(), 'limit' => $limit, 'offset' => $offset)
+			);
+print_r($result);
+
+```
---
+++ src/SharpSpring.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace AngeloPublio;
+/**
+ * Super-simple SharpSpring API v1 wrapper, in PHP.
+ * 
+ * @author Angelo Públio <angelopublio.com> 
+ * @version 0.1
+ */
+
+
+class SharpSpring
+{
+
+	private $accountID;
+	private $secretKey;
+    
+    
+    public function __construct($accountID, $secretKey)
+    {
+        $this->accountID = $accountID;
+        $this->secretKey = $secretKey;
+    }
+    
+    public function call($method, $args=array())
+    {
+        return $this->makeRequest($method, $args);
+    }
+
+    private function makeRequest($method, $args=array())
+    {      
+
+
+		$queryString = http_build_query(array('accountID' => $this->accountID, 'secretKey' => $this->secretKey)); 
+		$url = "http://api.sharpspring.com/pubapi/v1/?$queryString"; 
+
+		if (!session_id())
+		    session_start();
+    
+		$requestID = session_id();
+		
+		$data = array(                                                                                
+		   'method' => $method,                                                                      
+		   'params' => $args,                                                                      
+		   'id' => $requestID,                                                                       
+		);                                                                                            
+
+
+        if (function_exists('curl_init') && function_exists('curl_setopt')){
+			                                                                                             
+			$data = json_encode($data);                                                                   
+			$ch = curl_init($url);                                                                        
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                              
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);                                                  
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                               
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                   
+			   'Content-Type: application/json',                                                         
+			   'Content-Length: ' . strlen($data)                                                        
+			));                                                                                           
+
+			$result = curl_exec($ch);                                                                     
+			curl_close($ch);                                                                              
+
+			print_r($result);
+			exit;
+            
+        } else {
+            $json_data = json_encode($args);
+            $result    = file_get_contents($url, null, stream_context_create(array(
+                'http' => array(
+                    'protocol_version' => 1.1,
+                    'user_agent'       => 'PHP-MCAPI/2.0',
+                    'method'           => 'POST',
+                    'header'           => "Content-type: application/json\r\n".
+                                          "Connection: close\r\n" .
+                                          "Content-length: " . strlen($json_data) . "\r\n",
+                    'content'          => $json_data,
+                ),
+            )));
+        }
+
+        return $result ? json_decode($result, true) : false;
+    }
+
+}
+?>
